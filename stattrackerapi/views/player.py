3738c12026bf25837e5afdd1c6c9bb489839c6eb
Evan Alford <evan.al4d@gmail.com>
2026-05-13 09:21:27 -0500

    added create player function

--- stattrackerapi/views/player.py
+++ stattrackerapi/views/player.py
@@ -22,3 +22,21 @@ def get_players(request):
             'birthday': player.birthday,
         })
     return Response(player_list, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_players(request):
+    """Creates a new player and assigns them to the authenticated coach """
+    position_id = request.data.get('position_id')
+    name = request.data.get('name')
+    jersey_number = request.data.get('jersey_number')
+    birthday = request.data.get('birthday')
+
+    if not name or not jersey_number:
+        return Response(
+            {'error': 'Name and jersey number required'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    player =Player.objects.create(
+        coach=request.user
+    )
